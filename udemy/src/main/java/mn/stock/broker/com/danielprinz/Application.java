4d0dbca393478b903f701011553fe852c84f2cf8
brunodixini <bruno.mendonca@bancointer.com.br>
2021-10-06 16:34:35 -0300

    first commit

---
+++ udemy/src/main/java/mn/stock/broker/com/danielprinz/Application.java
@@ -0,0 +1,10 @@
+package mn.stock.broker.com.danielprinz;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
