ffaaf3d38323125fc02d3b1bcc0c5e21cc7a7b1f
brunodixini <bruno.mendonca@bancointer.com.br>
2021-10-08 15:58:50 -0300

    adding log at controller (INFO) and service (DEBUG)

--- udemy/src/main/java/mn/stock/broker/com/danielprinz/HelloWorldService.java
+++ udemy/src/main/java/mn/stock/broker/com/danielprinz/HelloWorldService.java
@@ -1,13 +1,24 @@
 package mn.stock.broker.com.danielprinz;
 
 import io.micronaut.context.annotation.Value;
+import io.micronaut.context.event.StartupEvent;
+import io.micronaut.runtime.event.annotation.EventListener;
 import jakarta.inject.Singleton;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Singleton
 public class HelloWorldService {
 
+    private static final Logger LOG = LoggerFactory.getLogger(HelloWorldService.class);
+
     @Value("${hello.service.greeting}")
     private String greeting;
+
+    @EventListener
+    public void onStartup(StartupEvent startupEvent) {
+        LOG.debug("Startup: {}", HelloWorldService.class.getSimpleName());
+    }
 
     public String sayHi() {
         return greeting;
