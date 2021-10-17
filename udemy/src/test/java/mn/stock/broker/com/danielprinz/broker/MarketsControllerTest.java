e918b8d8d865be62dfe2377394a9e970a8f3afef
brunodixini <bruno.mendonca@bancointer.com.br>
2021-10-17 18:39:34 -0300

    get endpoint and tests

--- udemy/src/test/java/mn/stock/broker/com/danielprinz/broker/MarketsControllerTest.java
+++ udemy/src/test/java/mn/stock/broker/com/danielprinz/broker/MarketsControllerTest.java
@@ -5,10 +5,13 @@ import io.micronaut.http.client.annotation.Client;
 import io.micronaut.runtime.EmbeddedApplication;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import jakarta.inject.Inject;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.LinkedHashMap;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @MicronautTest
@@ -23,7 +26,10 @@ class MarketsControllerTest {
 
     @Test
     void returnListOfMarkets() {
-        final List result = client.toBlocking().retrieve("/markets", List.class);
+        final List<LinkedHashMap<String, String>> result = client.toBlocking().retrieve("/markets", List.class);
         assertEquals(7, result.size());
+        assertThat(result)
+                .extracting(entry -> entry.get("value"))
+                .containsExactlyInAnyOrder("AAPL", "AMZN", "FB", "GOOG", "MSFT", "NFLX", "TSLA");
     }
 }
